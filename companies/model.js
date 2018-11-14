b0e037c14a34b4fd8139022b78d21e8d53732a62
JENDANG <JENNY.DANG117@GMAIL.COM>
2018-11-14 12:33:22 +0100

    companies add

---
+++ companies/model.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../db')
+
+const Company = sequelize.define('companies',{
+    name: {
+        type: Sequelize.STRING,
+        field: 'company_name',
+        allowNull: false
+    },
+    foundingYear: {
+        type: Sequelize.INTEGER,
+        field: 'founding_year',
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        field: 'company_description',
+        allowNull: false
+    }
+},
+    {
+    timestamps: false,
+    tableName: 'companies'
+    }
+
+)
+
+module.exports = Company
